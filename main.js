42c9ac7d5c0c929096ee528d2211626315a29c6e
keduoli-lovely <dawsonanabel243@gmail.com>
2023-09-03 23:34:55 +0800

    现实自定义指令,标签动态title

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import '@/utils/color.scss'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+
 // const app = createApp(App)
 // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 //   app.component(key, component)
@@ -12,19 +13,20 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // app.use(ElementPlus)
 
-
-
 import { createSSRApp } from 'vue';
 import * as Pinia from 'pinia';
 
 export function createApp() {
 	const app = createSSRApp(App);
-	
+
 	for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 	  app.component(key, component)
 	}
 	app.use(ElementPlus)
 	app.use(Pinia.createPinia());
+	app.directive('title', (el, binding) => {
+	  el.title = binding.value
+	})
 	return {
 		app,
 		Pinia, // 此处必须将 Pinia 返回
