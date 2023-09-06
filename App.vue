f84014fbe8bf0ef8a9b16fe434f4be42e12680fc
keduoli-lovely <dawsonanabel243@gmail.com>
2023-09-06 20:35:52 +0800

    判断是否选择过语言,在显示相关

--- App.vue
+++ App.vue
@@ -1,7 +1,22 @@
 <script>
+	import { lang_sel } from '@/store/lang_selec.js'
+	import { changeColor } from '@/store/changeColor_night.js'
+	
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			let { change_lang } = lang_sel()
+			let { change_color } = changeColor()
+			let defaultSetting = uni.getStorageSync('default')
+			if(defaultSetting) {
+				change_lang(defaultSetting.lang)
+				change_color(defaultSetting.color)
+			}else {
+				change_lang(1)
+				uni.setStorageSync('default', {
+					lang: 1,
+					color: 2
+				})
+			}
 		},
 		onShow: function() {
 			console.log('App Show')
