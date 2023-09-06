5747e2a061862860d3ae282ba45342b283228eab
keduoli-lovely <dawsonanabel243@gmail.com>
2023-09-06 20:36:43 +0800

    完善语言切换

--- pages/index/components/other.vue
+++ pages/index/components/other.vue
@@ -2,23 +2,23 @@
 	<el-card>
 		<view class="other">
 			<view class="title_setting">
-				设置
+				{{ lang["OTHER_SETTING_TITLE"] }}
 			</view>
 			<view class="setting_box">
 				<!-- // admin -->
 				<view class="admin">
-					<span class="admin_text">管理员账号:</span> 
+					<span class="admin_text">{{ lang["OTHER_ADMIN_ACCOUNT"] }}:</span> 
 					<view class="remove">
-						重置
+						{{ lang["OTHER_ADMIN_REBOOT"] }}
 					</view>
 					<view class="set_admin">
-						更改
+						{{ lang["OTHER_ADMIN_CHANGE"] }}
 					</view>
 				</view>
 				<!-- // 设置颜色 -->
-				<view class="back_color">
+				<view class="back_color" :class="default_lang === 2 ? 'back_color_atv' : ''">
 					<view class="appearance">
-						外观:
+						{{ lang["OTHER_COLOR"] }}:
 					</view>
 					<view class="back" 
 						:class="backColor_sta == 1 ? 'color_atv' : ''"
@@ -27,7 +27,7 @@
 						<view class="back_pic pic">
 							
 						</view>
-						深色
+						{{ lang["OHTER_COLOR_BACK"] }}
 					</view>
 					
 					<view class="white"
@@ -37,13 +37,13 @@
 						<view class="white_pic pic">
 							
 						</view>
-						浅色
+						{{ lang["OHTER_COLOR_WHITE"] }}
 					</view>
 					
 				</view>
-				<view class="select">
+				<view class="select" :class="default_lang === 2 ? 'select_atv' : ''">
 					 	<label class="radio1" @click="backColor_sta = 3">
-					 		<text class="radio_text">自动</text>
+					 		<text class="radio_text">{{ lang["OTHER_COLOR_AUTO"] }}</text>
 							<span class="radio_cont" 
 								:class="backColor_sta == 3 ? 'radio_atv' : ''"
 							>
@@ -52,7 +52,7 @@
 					 	</label>
 						<br />
 					 	<label class="radio2" @click="backColor_sta = 4">
-					 		<text class="radio_text">跟随系统</text>
+					 		<text class="radio_text">{{ lang["OTHER_COLOR_SYSTEM"] }}</text>
 							<span class="radio_cont"
 								:class="backColor_sta == 4 ? 'radio_atv' : ''"
 							>
@@ -62,10 +62,10 @@
 				</view>
 				
 				<!-- // 设置语言 -->
-				<view class="lang">
-					<span class="lang_text">语言选择:</span> <select v-model="lang_sta" class="select_row">
-						<option value="1">简体中文</option>
-						<option value="2">English</option>
+				<view class="lang" :class="default_lang === 2 ? 'lang_atv' : ''">
+					<span class="lang_text">{{ lang["OTHER_LANG_SELECT"] }}:</span> <select v-model="lang_sta" class="select_row">
+						<option :value="1">简体中文</option>
+						<option :value="2">English</option>
 					</select>
 				</view>
 			</view>
@@ -73,7 +73,7 @@
 			
 			<view class="userinfo_set" id="userinfo">
 				<view class="userinfo_text">
-					管理员详情
+					{{ lang["OTHER_SETTING_SUBTITLE"] }}
 				</view>
 				<view class="root_pic">
 					<image src="/public/bg.jpg" mode="aspectFill"></image>
@@ -84,7 +84,7 @@
 				</view>
 				
 				<view class="root_name">
-					<span class="name_text">昵称:</span><text class="change_name" title="修改昵称">admin</text>
+					<span class="name_text">{{ lang["OTHER_USER_NAME"] }}:</span><text class="change_name" v-title="lang['OTHER_USER_NAME_TITLE']">admin</text>
 				</view>
 				
 			</view>
@@ -96,12 +96,12 @@
 			 * @todo2 当用户修改但是没有保存, 弹出提示框让用户选择
 			 */ -->
 			<view class="save" v-if="save_btn_sta">
-				<view class="save_yes">
-					确认修改
+				<view class="save_yes" @click="change_lang_color">
+					{{ lang["OTHER_BTN_YES"] }}
 				</view>
 				
-				<view class="save_no">
-					取消修改
+				<view class="save_no" @click="save_btn_sta = false">
+					{{ lang["OTHER_BTN_NO"] }}
 				</view>
 			</view>
 		</view>
@@ -109,23 +109,53 @@
 </template>
 
 <script setup>
-	import { ref, watch } from 'vue'
+	import { ref, watch, onMounted } from 'vue'
+	import { storeToRefs } from 'pinia'
+	import { lang_sel } from '@/store/lang_selec.js'
+	import { changeColor } from '@/store/changeColor_night.js'
 	
 	
-	
+	// 语言切换
+	let { lang, default_lang } = storeToRefs(lang_sel())
+	let { default_color, night } = storeToRefs(changeColor())
+	let { change_lang } = lang_sel()
+	let { change_color } = changeColor()
 	// 主题切换
 	let backColor_sta = ref(2)
 	let lang_sta= ref(1)
-	
+	backColor_sta.value = default_color.value
+	lang_sta.value = default_lang.value
 	// 确认修改按钮的显示
 	let save_btn_sta = ref(false)
+	
+	onMounted(() => {
+		if(night.value) {
+			backColor_sta.value = 1
+		}
+	})
+	
 	watch([backColor_sta, lang_sta], () => {
-		if(backColor_sta.value != 2 || lang_sta.value != 1) {
+		if(backColor_sta.value != default_color.value || lang_sta.value != default_lang.value) {
 			save_btn_sta.value = true
 		}else {
 			save_btn_sta.value = false
 		}
 	})
+	
+	const change_lang_color = () => {
+		let sta = uni.getStorageSync('default')
+		if(backColor_sta.value != default_color.value) {
+			sta.color = backColor_sta.value
+			default_color.value = backColor_sta.value
+			change_color(backColor_sta.value)
+		}else if(lang_sta.value != default_lang.value) {
+			sta.lang = lang_sta.value
+			default_lang.value = lang_sta.value
+			change_lang(lang_sta.value)
+		}
+		uni.setStorageSync('default', sta)
+		save_btn_sta.value = false
+	}
 </script>
 
 <style lang="scss" scoped>
@@ -172,6 +202,9 @@
 				}
 			}
 			
+			.back_color_atv {
+				padding-left: 60rpx !important;
+			}
 			.back_color {
 				display: flex;
 				padding-left: 120rpx;
@@ -208,6 +241,9 @@
 				}
 			}
 			
+			.select_atv {
+				margin: 30rpx 0 80rpx 324rpx !important;
+			}
 			.select {
 				margin: 30rpx 0 80rpx 245rpx;
 				.radio1,
@@ -239,6 +275,9 @@
 				}
 			}
 			
+			.lang_atv {
+				padding-left: 95rpx;
+			}
 			.lang {
 				padding-left: 40rpx;
 				display: flex;
